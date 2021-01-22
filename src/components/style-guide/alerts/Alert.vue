8942198a66cfeccafdb2e03ab57e70c766e3c5ab
Daniel Barrios <daniel.barrios@dox.cl>
2021-01-21 22:59:48 -0300

    Alert component

--- src/components/style-guide/alerts/Alert.vue
+++ src/components/style-guide/alerts/Alert.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <v-alert
-        :icon="false"
+        :type="type"
+        :icon="iconByType()"
         tile
         v-bind="$attrs"
         v-on="$listeners">
@@ -16,5 +17,25 @@ import './Alert.scss';
 export default {
   name: 'Alert',
   inheritAttrs: false,
+  methods: {
+    iconByType: function () {
+      if (this.$props.type === 'info') {
+        return 'mdi-bell-outline';
+      }
+
+      if (this.$props.type === 'warning') {
+        return 'mdi-error-outline';
+      }
+
+      if (this.$props.type === 'success') {
+        return 'mdi-tick-circle-outline';
+      }
+
+      return false;
+    }
+  },
+  props: {
+    type: String
+  }
 }
 </script>
