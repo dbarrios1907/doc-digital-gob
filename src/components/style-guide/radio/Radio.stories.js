4102f5f50492429819c085293fdb5f8def79ffee
Daniel Barrios <daniel.barrios@dox.cl>
2021-01-26 01:38:16 -0300

    feat(Radio): Add Radio Story

---
+++ src/components/style-guide/radio/Radio.stories.js
@@ -0,0 +1,51 @@
+export default {
+  title: 'Style Guide/Radio',
+  argTypes: {
+    color: { control: { type: 'select', options: ['primary', 'secondary', 'neutral'] } },
+    size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  data () {
+    return {
+      column: null,
+      row: null,
+    }
+  },
+  template: `
+    <v-container fluid>
+      <v-radio-group
+          v-model="column"
+          column
+      >
+        <v-radio
+            label="Option 1"
+            value="radio-1"
+        ></v-radio>
+        <v-radio
+            label="Option 2"
+            value="radio-2"
+        ></v-radio>
+      </v-radio-group>
+      <hr>
+      <v-radio-group
+          v-model="row"
+          row
+      >
+        <v-radio
+            label="Option 1"
+            value="radio-1"
+        ></v-radio>
+        <v-radio
+            label="Option 2"
+            value="radio-2"
+        ></v-radio>
+      </v-radio-group>
+    </v-container>
+  `
+});
+
+export const Default = Template.bind({});
+Default.args = {};
