591635d1ceb999d53a2d7fc1bd6046d3cc25552e
Daniel Barrios <daniel.barrios@dox.cl>
2021-01-26 01:41:15 -0300

    feat(Rating): Add Ratings Story

---
+++ src/components/style-guide/ratings/Ratings.vue
@@ -0,0 +1,28 @@
+<template>
+<!--  <v-rating-->
+<!--      color="primary"-->
+<!--      empty-icon="$mdiStarOutline"-->
+<!--      full-icon="$mdiStar"-->
+<!--      half-icon="$mdiStarHalfFull"-->
+<!--      half-increments-->
+<!--      hover-->
+<!--      length="5"-->
+<!--  ></v-rating>-->
+
+  <v-rating
+      color="primary"
+      background-color="primary"
+      half-increments
+      hover
+      dense
+      :ripple="false"
+      v-bind="$attrs" v-on="$listeners"
+  ></v-rating>
+</template>
+
+<script>
+export default {
+  name: 'doc-ratings',
+  inheritAttrs: false,
+}
+</script>
