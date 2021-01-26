35664b4bbd7eef85365a4f16210fa28421307403
Daniel Barrios <daniel.barrios@dox.cl>
2021-01-26 01:39:38 -0300

    feat(Switch): Add Switch story

---
+++ src/components/style-guide/switch/Switch.stories.js
@@ -0,0 +1,47 @@
+export default {
+  title: 'Style Guide/Switch',
+  argTypes: {
+    color: { control: { type: 'select', options: ['primary', 'secondary', 'neutral'] } },
+    size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  data() {
+    return {
+      switch1: true,
+      switch2: false,
+      switch3: false,
+    }
+  },
+  template: `
+    <v-sheet class="pa-5">
+      <v-switch
+          v-model="switch1"
+          inset
+          :ripple="false"
+          dense
+          :label="'Switch 1: '+ switch1.toString()"
+      ></v-switch>
+      <v-switch
+          v-model="switch2"
+          inset
+          dense
+          :ripple="false"
+          :label="'Switch 2: ' + switch2.toString()"
+      ></v-switch>
+      <v-switch
+          v-model="switch3"
+          color="#04A398"
+          inset
+          dense
+          :ripple="false"
+          :label="'Switch 3: ' + switch3.toString()"
+      ></v-switch>
+    </v-sheet>
+  `
+});
+
+export const Default = Template.bind({});
+Default.args = {};
