0ea4a244d9a76477e6715419063cbeb4e28066f2
Daniel Barrios <daniel.barrios@dox.cl>
2021-01-26 01:37:24 -0300

    feat(Checkbox): Add checkbox Story

---
+++ src/components/style-guide/checkbox/Checkbox.stories.js
@@ -0,0 +1,49 @@
+export default {
+  title: 'Style Guide/Checkbox',
+  argTypes: {
+    color: { control: { type: 'select', options: ['primary', 'secondary', 'neutral'] } },
+    size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  data() {
+    return {
+      selected: []
+    }
+  },
+  template: `
+    <v-container fluid>
+      <p>{{ selected }}</p>
+      <v-checkbox
+          v-model="selected"
+          :ripple="false"
+          indeterminate
+          label="Text de Ejemplo 1"
+          value="Text1"
+      ></v-checkbox>
+      <v-checkbox
+          v-model="selected"
+          :ripple="false"
+          label="Text de Ejemplo 2"
+          value="Text2"
+      ></v-checkbox>
+      <v-checkbox
+          v-model="selected"
+          :ripple="false"
+          label="Text de Ejemplo 3"
+          value="Text3"
+      ></v-checkbox>
+      <v-checkbox
+          v-model="selected"
+          :ripple="false"
+          label="Text de Ejemplo 4"
+          value="Text4"
+      ></v-checkbox>
+    </v-container>
+  `
+});
+
+export const Default = Template.bind({});
+Default.args = {};
