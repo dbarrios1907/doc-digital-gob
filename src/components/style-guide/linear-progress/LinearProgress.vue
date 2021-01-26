de1e3e2feadf068c195f6ca47119717b80d00d29
Daniel Barrios <daniel.barrios@dox.cl>
2021-01-26 01:41:01 -0300

    feat(LinearProgress): Add LinearProgress Story

---
+++ src/components/style-guide/linear-progress/LinearProgress.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-progress-linear
+      color="#0F69C4"
+      background-color="#CFE1F3"
+      v-model="knowledge"
+      height="25"
+      class="neutral--text"
+      v-bind="$attrs"
+      v-on="$listeners"
+  >
+    <strong>{{ Math.ceil(knowledge) }}%</strong>
+  </v-progress-linear>
+</template>
+
+<script>
+export default {
+  name: 'doc-linear-progress',
+  inheritAttrs: false,
+}
+</script>
