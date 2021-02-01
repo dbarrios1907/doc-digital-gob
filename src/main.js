6bb4fd2059410e03ad7b10dab758681e415e5062
Daniel Barrios <daniel.barrios@dox.cl>
2021-02-01 04:26:37 +0000

    feat(architecture), add router and vuex store management

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import router from './plugins/vue-router';
+import store from './plugins/vuex';
 import './plugins/vee-validate';
 import '@babel/polyfill'
 
@@ -8,5 +10,7 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  router,
+  store: store,
+  render: h => h(App),
 }).$mount('#app')
