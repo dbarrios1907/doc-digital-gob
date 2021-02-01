d1bbd0a7ea6426d204eec033c52d1af720188b0e
Daniel Barrios <daniel.barrios@dox.cl>
2021-01-31 23:24:31 -0300

    feat(Storybook): update Table styles (WIP)

--- src/components/style-guide/data-table/DataTable.vue
+++ src/components/style-guide/data-table/DataTable.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-data-table v-bind="$attrs" v-on="$listeners"><slot></slot></v-data-table>
+  <v-data-table v-bind="$attrs" v-on="$listeners">
+    <!-- Pass on all named slots -->
+    <slot v-for="slot in Object.keys($slots)" :name="slot" :slot="slot"/>
+
+    <!-- Pass on all scoped slots -->
+    <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope"><slot :name="slot" v-bind="scope"/></template>
+  </v-data-table>
 </template>
 
 <script>
 import './DataTable.scss';
 export default {
   name: 'DataTable',
-  inheritAttrs: false,
+  inheritAttrs: true,
 }
 </script>
