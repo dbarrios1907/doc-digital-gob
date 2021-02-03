71ce2b0b8ebbbe2d3f899c8bce11805cc23fe6bb
Daniel Barrios <daniel.barrios@dox.cl>
2021-02-03 00:48:38 -0300

    feat(Storybook): Add upload component (WIP)

---
+++ src/components/style-guide/upload/index.js
@@ -0,0 +1,12 @@
+import BUpload from './upload.vue'
+import BFileList from './components/file-list.vue'
+import { pluginFactory } from '../../../lib/utils/plugins'
+
+const BUploadPlugin = /*#__PURE__*/ pluginFactory({
+  components: {
+    BUpload,
+    BFileList,
+  }
+})
+
+export { BUploadPlugin, BUpload, BFileList }
