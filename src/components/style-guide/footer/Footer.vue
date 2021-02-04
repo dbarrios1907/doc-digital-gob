c4de7a3f6511c7e3837995c9547fed484327800d
Leonardo Enrique Reyna Castro <leonardo.reyna@dox.cl>
2021-02-04 20:35:52 -0300

    nuevo componente footer

---
+++ src/components/style-guide/footer/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-footer class="dark" padless>
+    <v-col class="text-left py-0" cols="4">
+      <div class="pl-4 pt-3 pb-3">
+        <span :style="{color: dcolor}" class="line-height-16 font-nunito font-small weight-300"> Versión 2.1.11 </span>
+      </div>
+      <BrandWatermark />
+    </v-col>
+
+    <v-col class="text-right" cols="8">
+      <span :style="{color: dcolor}" class="line-height-16 font-nunito font-small weight-300">
+        Para conocer el detalle de la versión actual y actualizaciones anteriores
+        <a href="#" class="weight-600" :style="{color: dcolor}">revise aquí</a>
+      </span>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'DxFooter',
+  inheritAttrs: false,
+  computed: {
+    dcolor() {
+      if (this.color) return this.color
+      return this.$vuetify.theme.isDark ? this.$vuetify.theme.themes.dark.dark : this.$vuetify.theme.themes.light.light
+    },
+  },
+  props: {
+    color: String,
+  }
+}
+</script>
--- src/components/style-guide/svg/index.js
+++ src/components/style-guide/svg/index.js
@@ -1,2 +1,2 @@
 export { default as HeaderLogo } from './components/HeaderLogo/HeaderLogo.svg'
-// export { default as BrandWatermark } from './components/BrandWatermark/BrandWatermark.svg'
+export { default as BrandWatermark } from './components/BrandWatermark/BrandWatermark.svg'
