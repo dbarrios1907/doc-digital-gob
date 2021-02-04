90aa971c6a5a13b003ba8cdac42647798bc19571
Daniel Barrios <daniel.barrios@dox.cl>
2021-02-04 17:55:24 -0300

    feat(Storybook): Update upload component

--- src/components/style-guide/breadcrumbs/Breadcrumbs.stories.js
+++ src/components/style-guide/breadcrumbs/Breadcrumbs.stories.js
@@ -1,37 +1,31 @@
-import Breadcrumbs from './Breadcrumbs';
 export default {
   title: 'Style Guide/Breadcrumbs',
-  component: Breadcrumbs,
-  argTypes: {
-  },
+  argTypes: {},
 }
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { Breadcrumbs },
   template: `
-    <div class="bc-container">
-          <a style="float:left">←</a>
-          <Breadcrumbs :items="items" />
-    </div>
-  `
-});
+    <dx-breadcrumbs :items="items" />
+  `,
+})
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
   items: [
-  {
-    text: 'Administración',
-    disabled: false,
-    href: 'breadcrumbs_dashboard',
-  },
-  {
-    text: 'Documentos',
-    disabled: false,
-    href: 'breadcrumbs_link_1',
-  },
-  {
-    text: 'Enviados Pendientes',
-    disabled: true,
-    href: 'breadcrumbs_link_2',
-  },
-], };
+    {
+      text: 'Administración',
+      disabled: false,
+      href: 'breadcrumbs_dashboard',
+    },
+    {
+      text: 'Documentos',
+      disabled: false,
+      href: 'breadcrumbs_link_1',
+    },
+    {
+      text: 'Enviados Pendientes',
+      disabled: true,
+      href: 'breadcrumbs_link_2',
+    },
+  ],
+}
--- src/components/style-guide/breadcrumbs/Breadcrumbs.vue
+++ src/components/style-guide/breadcrumbs/Breadcrumbs.vue
@@ -1,18 +1,32 @@
 <template>
-  <div>
-    <v-breadcrumbs :items="items"></v-breadcrumbs>
+  <div class="d-flex-inline">
+    <template v-if="firstItem">
+      <span :href="firstItem.href">
+        <dx-icon dense color="primary">mdi-arrow-left</dx-icon>
+      </span>
+      <v-breadcrumbs class="d-inline pl-1 pr-2" :items="items" v-bind="$attrs" v-on="$listeners">
+        <template v-slot:item="{ item }">
+          <v-breadcrumbs-item :to="item.href" class="primary--text" active-class="dark--text">
+            {{ item.text }}
+          </v-breadcrumbs-item>
+        </template>
+      </v-breadcrumbs>
+    </template>
   </div>
 </template>
 
 <script>
-  import './Breadcrumbs.scss'
-  export default {
-    name: 'BreadCrumbs',
-    inheritAttrs: false,
-    props: {
-      items: Array()
-    }
-  }
+import './Breadcrumbs.scss'
+export default {
+  name: 'DxBreadcrumbs',
+  inheritAttrs: false,
+  props: {
+    items: Array,
+  },
+  computed: {
+    firstItem() {
+      return this.items.length > 0 ? this.items[0] : undefined
+    },
+  },
+}
 </script>
-
-
