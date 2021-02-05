82baeb15b11d2a42e7c663ad8e09c3bdb4c57209
Leonardo Enrique Reyna Castro <leonardo.reyna@dox.cl>
2021-02-05 01:50:51 -0300

    agregando componente para cambiar color y tamaño fuente

---
+++ src/components/style-guide/icons-actions/IconsActions.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <dx-button color="#656565" outlined height="38px" min-width="18" width="38" small v-bind="$props" @click="changetheme">
+      <v-icon regular>mdi-dock-left</v-icon>
+    </dx-button>
+
+    <dx-button color="#656565" outlined height="38px" min-width="18" width="38" small v-bind="$props" @click="resize('minus')">
+      <v-icon>mdi-format-annotation-minus</v-icon>
+    </dx-button>
+
+    <dx-button color="#656565" outlined height="38px" min-width="18" width="38" small v-bind="$props" @click="resize('plus')">
+      <v-icon>mdi-format-annotation-plus</v-icon>
+    </dx-button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'DxIconsActions',
+  inheritAttrs: false,
+  methods: {
+    resize(action) {
+      var sz = document.body.style.fontSize, size;
+      if (sz =='') sz = 14;
+      if (action == "minus") size = parseFloat(sz) - 1;
+      if (action == "plus") size = parseFloat(sz) + 1;
+      document.body.style.fontSize = size + 'px';
+    },
+    changetheme (){
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    }
+  }
+}
+</script>
