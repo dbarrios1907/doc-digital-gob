1c4384df39e58b4435a76fe2c45c931fd7759013
Yasmany <ybreff87@gmail.com>
2021-02-05 14:30:08 -0600

    elementos del dashboard

---
+++ src/components/style-guide/dashboard-items/DashbItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-card class="mx-auto px-3" elevation=0 max-width="344">
+      <slot />
+    </v-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DxDashbitem',
+  inheritAttrs: false,
+  props:{
+    color: String, 
+  }
+}
+</script>
