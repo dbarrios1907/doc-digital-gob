91650b421e47d9f8760db75ca04228171c175a4c
Daniel Barrios <daniel.barrios@dox.cl>
2021-02-06 22:32:48 -0300

    feat(Framework Foundation): Update font, styles, components layouts

--- src/components/style-guide/layout/components/dashboard-items/DashbItem.stories.js
+++ src/components/style-guide/layout/components/dashboard-items/DashbItem.stories.js
@@ -1,29 +1,32 @@
 export default {
-  title: 'Style Guide/DashbItems',
+  title: 'Style Guide/layout/Components/Dashboard Items',
   argTypes: {},
 }
 
 export const Demo = () => ({
+  data: () => ({
+    itemClasses: 'px-3 primary--text',
+  }),
   template: `
     <div>
       <v-container> 
         <v-row>
           <v-col cols="3" class="ml-12">
-            <dx-dashbitem style="color:#0B36AA" class="px-3" textheader="9.999" titleheader="Usuarios activos" messaje="" />
+            <dx-dashbitem class="itemClasses" textheader="9.999" titleheader="Usuarios activos" messaje="" />
           </v-col>
         </v-row>
         <v-divider class="mt-5" inset></v-divider>
         <v-row class="mt-3">
           <v-col cols="3" class="mx-lg-auto">
-            <dx-dashbitem style="color:#0B36AA" class="px-3" textheader="9.999" titleheader="Documentos enviados" messaje="" />
+            <dx-dashbitem :class="itemClasses" textheader="9.999" titleheader="Documentos enviados" messaje="" />
           </v-col>
           <v-divider inset vertical></v-divider>
           <v-col cols="3" class="mx-lg-auto">
-          <dx-dashbitem style="color:#0B36AA" class="px-3" textheader="9.999" titleheader="Documentos recibidos" messaje="15 pendientes de recibir" />
+          <dx-dashbitem :class="itemClasses" textheader="9.999" titleheader="Documentos recibidos" messaje="15 pendientes de recibir" />
           </v-col>
           <v-divider inset vertical></v-divider>
           <v-col cols="3" class="mx-lg-auto">
-            <dx-dashbitem style="color:#0B36AA" class="px-3" textheader="9.999" titleheader="Documentos pendientes" messaje="" />
+            <dx-dashbitem :class="itemClasses" textheader="9.999" titleheader="Documentos pendientes" messaje="" />
         </v-col>
         </v-row>
       </v-container>
