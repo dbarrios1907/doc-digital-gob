33392ffe636ad0393d67e794d8c712b87a748941
Leonardo Enrique Reyna Castro <leonardo.reyna@dox.cl>
2021-02-08 04:03:37 -0300

    fix(errorpage): arreglando conflicto

--- src/components/style-guide/layout/Error.stories.js
+++ src/components/style-guide/layout/Error.stories.js
@@ -1,25 +1,13 @@
 export default {
   title: 'Style Guide/Layout',
-<<<<<<< HEAD
   argTypes: {},
-=======
   argTypes: {
     color: { control: {type: 'color'} },
   },
->>>>>>> 635f4fa46039c512bd21bfbdb5314ef755c0fac9
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-<<<<<<< HEAD
-=======
-  data() {
-    return {
-      page: 1,
-      pageCount: 3,
-    }
-  },
->>>>>>> 635f4fa46039c512bd21bfbdb5314ef755c0fac9
   template: `
     <ErrorPage />
   `
