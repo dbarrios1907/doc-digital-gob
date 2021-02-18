14ba6bca089c1c658a6d41dc7830ecdc86e051af
Daniel Barrios <daniel.barrios@dox.cl>
2021-02-18 01:39:41 -0300

    build(Button): Update button styles

--- src/components/style-guide/button/Button.stories.js
+++ src/components/style-guide/button/Button.stories.js
@@ -14,28 +14,40 @@ const Template = (args, { argTypes }) => ({
       <h3>Primary</h3>
       <v-row>
         <v-col>
-          <dx-button color="primary" small v-bind="$props"> {{label}} </dx-button>
-          <dx-button color="primary" v-bind="$props"> {{label}}  </dx-button>
-          <dx-button color="primary" rounded v-bind="$props">
-            <dx-icon left>
-              mdi-pencil
+          <dx-button color="primary" small v-bind="$props">
+            <logo-clave-unica-icon color="light" pr="5"/>
+            <span class="underline-text">{{label}}</span>
+          </dx-button>
+          <dx-button color="primary" v-bind="$props">
+            <logo-clave-unica-icon color="light"  pr="5" />
+            <span class="underline-text">{{label}}</span> 
+          </dx-button>
+          <dx-button color="primary" outlined v-bind="$props">
+            <logo-clave-unica-icon color="primary"  pr="5" />
+            <span class="underline-text">{{label}}</span>
+            <dx-icon right chevron regular>
+              mdi-chevron-down
             </dx-icon>
-            {{label}}
+          </dx-button>
+          <dx-button large color="primary" v-bind="$props">
+            <logo-clave-unica-icon color="light" width="24" height="28" pr="8" />
+            <span class="underline-text">{{label}}</span>
+            <dx-icon right chevron regular>
+              mdi-chevron-down
+            </dx-icon>
+          </dx-button>
+          <dx-button color="primary" rounded v-bind="$props">
+            <logo-clave-unica-icon color="light"  pr="5" />
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="primary" fab v-bind="$props">
             <dx-icon dark>
               mdi-pencil
             </dx-icon>
           </dx-button>
-          <dx-button color="primary" disabled v-bind="$props"> {{label}} </dx-button>
+          <dx-button color="primary" disabled v-bind="$props"> <span class="underline-text">{{label}}</span></dx-button>
           <dx-button color="primary" text v-bind="$props">
-            {{label}}
-          </dx-button>
-          <dx-button color="primary" outlined v-bind="$props">
-            <span class="text-underline">{{label}} </span>
-            <dx-icon right regular>
-              mdi-chevron-down
-            </dx-icon>
+            <span class="underline-text">{{label}}</span>
           </dx-button>
         </v-col>
       </v-row>
@@ -43,62 +55,62 @@ const Template = (args, { argTypes }) => ({
       <h3>Secondary</h3>
       <v-row>
         <v-col>
-          <dx-button class="primary--text" color="secondary" v-bind="$props"> {{label}}  </dx-button>
+          <dx-button class="primary--text" color="secondary" v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button class="primary--text" color="secondary"  rounded v-bind="$props">
             <dx-icon left>
               mdi-pencil
             </dx-icon>
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button class="primary--text" color="secondary" fab v-bind="$props">
             <dx-icon dark>
               mdi-pencil
             </dx-icon>
           </dx-button>
-          <dx-button class="primary--text" color="secondary" disabled v-bind="$props"> {{label}}  </dx-button>
+          <dx-button class="primary--text" color="secondary" disabled v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
         </v-col>
       </v-row>
 
       <h3>Neutral</h3>
       <v-row>
         <v-col>
-          <dx-button color="neutral"  v-bind="$props"> {{label}}  </dx-button>
+          <dx-button color="neutral"  v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button color="neutral"  rounded v-bind="$props">
             <dx-icon left>
               mdi-pencil
             </dx-icon>
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="neutral"  fab v-bind="$props">
             <dx-icon dark>
               mdi-pencil
             </dx-icon>
           </dx-button>
-          <dx-button color="neutral"  disabled v-bind="$props"> {{label}} </dx-button>
+          <dx-button color="neutral"  disabled v-bind="$props"> <span class="underline-text">{{label}}</span></dx-button>
         </v-col>
       </v-row>
       
       <h3>Success</h3>
       <v-row>
         <v-col>
-          <dx-button color="success" v-bind="$props"> {{label}} </dx-button>
+          <dx-button color="success" v-bind="$props"> <span class="underline-text">{{label}}</span></dx-button>
           <dx-button color="success" rounded v-bind="$props">
             <dx-icon left>
               mdi-pencil
             </dx-icon>
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="success" fab v-bind="$props">
             <dx-icon dark>
               mdi-pencil
             </dx-icon>
           </dx-button>
-          <dx-button color="success" disabled v-bind="$props"> {{label}} </dx-button>
+          <dx-button color="success" disabled v-bind="$props"> <span class="underline-text">{{label}}</span></dx-button>
           <dx-button color="success" text v-bind="$props">
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="success" outlined v-bind="$props">
-            <span class="text-underline"> {{label}} </span>
+            <span class="text-underline"> <span class="underline-text">{{label}}</span></span>
             <dx-icon right regular>
               mdi-chevron-down
             </dx-icon>
@@ -109,24 +121,24 @@ const Template = (args, { argTypes }) => ({
       <h3>Error</h3>
       <v-row>
         <v-col>
-          <dx-button color="error" v-bind="$props"> {{label}}  </dx-button>
+          <dx-button color="error" v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button color="error" rounded v-bind="$props">
             <dx-icon left>
               mdi-pencil
             </dx-icon>
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="error" fab v-bind="$props">
             <dx-icon dark>
               mdi-pencil
             </dx-icon>
           </dx-button>
-          <dx-button color="error" disabled v-bind="$props"> {{label}}  </dx-button>
+          <dx-button color="error" disabled v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button color="error" text v-bind="$props">
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="error" outlined v-bind="$props">
-            <span class="text-underline"> {{label}} </span>
+            <span class="text-underline"> <span class="underline-text">{{label}}</span></span>
             <dx-icon right regular>
               mdi-chevron-down
             </dx-icon>
@@ -137,7 +149,7 @@ const Template = (args, { argTypes }) => ({
       <h3 class="m-3">Warning</h3>
       <v-row>
         <v-col>
-          <dx-button color="warning" v-bind="$props"> {{label}}  </dx-button>
+          <dx-button color="warning" v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button color="warning" rounded v-bind="$props">
             <dx-icon left>
               mdi-pencil
@@ -149,12 +161,12 @@ const Template = (args, { argTypes }) => ({
               mdi-pencil
             </dx-icon>
           </dx-button>
-          <dx-button color="warning"  disabled v-bind="$props"> {{label}}  </dx-button>
+          <dx-button color="warning"  disabled v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button color="warning" text v-bind="$props">
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="warning" outlined v-bind="$props">
-            <span class="text-underline">{{label}} </span>
+            <span class="text-underline"><span class="underline-text">{{label}}</span></span>
             <dx-icon right regular>
               mdi-chevron-down
             </dx-icon>
@@ -165,7 +177,7 @@ const Template = (args, { argTypes }) => ({
       <h3 class="m-3">Dark</h3>
       <v-row>
         <v-col>
-          <dx-button color="dark" v-bind="$props"> {{label}}  </dx-button>
+          <dx-button color="dark" v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button color="dark" rounded v-bind="$props">
             <dx-icon left>
               mdi-pencil
@@ -177,12 +189,12 @@ const Template = (args, { argTypes }) => ({
               mdi-pencil
             </dx-icon>
           </dx-button>
-          <dx-button color="dark" disabled v-bind="$props"> {{label}}  </dx-button>
+          <dx-button color="dark" disabled v-bind="$props"> <span class="underline-text">{{label}}</span> </dx-button>
           <dx-button color="dark" text v-bind="$props">
-            {{label}}
+            <span class="underline-text">{{label}}</span>
           </dx-button>
           <dx-button color="dark" outlined v-bind="$props">
-            <span class="text-underline"> {{label}} </span>
+            <span class="text-underline"> <span class="underline-text">{{label}}</span></span>
             <dx-icon right regular>
               mdi-chevron-down
             </dx-icon>
@@ -196,5 +208,5 @@ const Template = (args, { argTypes }) => ({
 export const Primary = Template.bind({})
 Primary.args = {
   color: 'primary',
-  label: 'Button',
+  label: 'Type something',
 }
